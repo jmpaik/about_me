c3d450d665225fb378b771fd3fc55634724d3d37
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-09-20 15:52:52 -0700

    wrote js code for all 5 questions

--- app.js
+++ app.js
@@ -11,10 +11,49 @@ else {
   console.log('Sorry that\'s incorrect.');
 }
 
+var answer2 = prompt('Does Michael practice writing code on a Macbook Pro?');
+answer2 = answer2.toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert('That\'s correct!');
+  console.log('That\'s correct!');
+}
+else {
+  alert ('Sorry that\'s incorrect.');
+  console.log('Sorry that\'s incorrect.');
+}
 
+var answer3 = prompt('Currently, can Michael build a complex website?');
+answer3 = answer3.toLowerCase();
+if (answer3 === 'no' || answer3 === 'n') {
+  alert('That\'s correct!');
+  console.log('That\'s correct!');
+}
+else {
+  alert ('Sorry that\'s incorrect.');
+  console.log('Sorry that\'s incorrect.');
+}
 
+var answer4 = prompt('Has Michael had any prior experience writing code?');
+answer4 = answer4.toLowerCase();
+if (answer4 === 'no' || answer4 === 'n') {
+  alert('That\'s correct!');
+  console.log('That\'s correct!');
+}
+else {
+  alert ('Sorry that\'s incorrect.');
+  console.log('Sorry that\'s incorrect.');
+}
 
-
+var answer5 = prompt('Will Michael pass Code Fellows 201 course? (Yes, No, or Maybe?)');
+answer5 = answer5.toLowerCase();
+if (answer5 === 'maybe' || answer5 === 'm') {
+  alert('That\'s correct!');
+  console.log('That\'s correct!');
+}
+else {
+  alert ('Sorry that\'s incorrect.');
+  console.log('Sorry that\'s incorrect.');
+}
 /*var usertalent = prompt('What is your talent?');
 alert('You must be gifted at ' + usertalent + '.');
 console.log('The user\'s talent is ' + usertalent);
