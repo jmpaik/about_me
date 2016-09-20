a57292d11a8eeaa219ff3df49bf2f936b07d34f2
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-09-20 13:30:54 -0700

    fixed script tags

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+var usertalent = prompt('What is your talent?');
+alert('You must be gifted at ' + usertalent + '.');
+console.log('The user\'s talent is ' + usertalent);
+
+
+var userfood = prompt('What is your favorite food?');
+alert('I enjoy eating ' + userfood + ' too.');
+console.log('The user\'s favorite food is ' + userfood);
+
+
+
+var usersport = prompt('What is your favorite sports event to watch?');
+alert('Being able to play ' + usersport + ' sounds difficult but easier to watch.');
+console.log('The user\'s favorite sport is ' + usersport);
+
+
+console.log('Wow, ' + username + ' you know how to ' + usertalent + ' and you probably like to eat ' + userfood + ' while watching ' + usersport + '.');
+document.write('Wow, ' + username + ' you know how to ' + usertalent + ' and you probably like to eat ' + userfood + ' while watching ' + usersport + '.');
