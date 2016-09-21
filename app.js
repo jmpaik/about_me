80344a34ee57006e04cc25fd4b3db648ae5b128a
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-09-21 16:38:09 -0700

    fixed syntax errors is js

--- app.js
+++ app.js
@@ -57,21 +57,23 @@ else {
 
 /*Add a sixth question to my guessing game that takes numeric input by prompting a user to guess a number (for instance, "What is my favorite number?" or "How many Pokémon did I catch yesterday?"), and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly four opportunities to get the correct answer, so that my fancy programming skills are showcased.*/
 for (var i = 0; i <= 3; i++) {
-    var guess = parseInt(prompt('What is my favorite number?'));
-    if (guess == 7) {
+    var guessNum = parseInt(prompt('What is my favorite number?'));
+    if (guessNum === 7) {
       alert('That\'s correct!');
       console.log('That\'s correct!');
       break;
-  } else if (guess < 7) {
+  } else if (guessNum < 7) {
       alert('Sorry that\'s too low. Try Again.');
       console.log('Sorry that\'s too low.');
-  } else if (guess > 7) {
+  } else if (guessNum > 7) {
       alert('Sorry that\'s too high. Try Again.');
       console.log('Sorry that\'s too high. Try Again.');
   }
 }
 /*add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.*/
-
+/*for (var i = 0; i <= 5; i++) {
+  var (guessState) = prompt('Can you guess a state that I have lived in besides Washington?');
+  if (guessState) === ['Washington', 'Illinois', 'New York', 'Virginia']
 
 
 
